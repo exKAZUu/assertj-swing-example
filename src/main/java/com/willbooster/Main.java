869858091e7aa6a21b2be037a89734651fb4cdc3
Sakamoto, Kazunori <exkazuu@gmail.com>
2022-11-07 14:30:59 +0900

    feat: make assertj-swing work

--- src/main/java/com/willbooster/Main.java
+++ src/main/java/com/willbooster/Main.java
@@ -3,5 +3,12 @@ package com.willbooster;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello");
+        showWindow().setSize(600, 600);
+    }
+
+    public static MainWindow showWindow() {
+        MainWindow mainWindow = new MainWindow();
+        mainWindow.setVisible(true);
+        return mainWindow;
     }
 }
